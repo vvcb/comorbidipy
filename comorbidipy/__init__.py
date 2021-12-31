2295581188894b0015d82306ea19347367d82040
vvcb <vvcb.n1@gmail.com>
2021-12-31 16:54:27 +0000

    Bump version to 0.2.0

--- comorbidipy/__init__.py
+++ comorbidipy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """vvcb"""
 __email__ = 'vvcb.n1@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .calculator import comorbidity, hfrs
 from .colnames import get_colnames
--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vvcb/comorbidipy",
-    version="0.1.0",
+    version="version='0.2.0'",
     zip_safe=False,
 )
