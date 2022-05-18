89507bdedc88675019e300ea8210f0c56c6e1545
vvcb <vvcb.n1@gmail.com>
2022-05-18 22:56:29 +0100

    Update test_comorbidity.py

--- tests/test_comorbidity.py
+++ tests/test_comorbidity.py
@@ -3,31 +3,95 @@
 """Tests for `comorbidipy` package."""
 
 
+import random
 import unittest
-from click.testing import CliRunner
+from itertools import cycle
 
-from comorbidipy import comorbidipy
-from comorbidipy import cli
+import pandas as pd
+
+from comorbidipy import comorbidity, mapping
+
+def generate_synthetic_icd10_data(
+    score: str = "charlson",
+    icd: str = "icd10",
+    variant: str = "shmi",
+    weighting: str = None,  # for compatibility
+):
+
+    mapping_key = f"{score}_{icd}_{variant}"
+    patients = []
+
+    num_of_patients = max(map(len, mapping.mapping[mapping_key].values()))
+    codes = [cycle(random.sample(v, len(v))) for v in mapping.mapping[mapping_key].values()]
+
+    for n in range(num_of_patients):
+
+        age = random.randint(0, 100)
+
+        for c in codes:
+
+            patients.append({"id": n, "age": age, "code": next(c)})
+
+    df = pd.DataFrame.from_records(patients)
+    return df
 
 
-class Testcomorbidipy(unittest.TestCase):
+class TestComorbidipy(unittest.TestCase):
     """Tests for `comorbidipy` package."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
 
+        self.test_args = [
+            {
+                "args": {
+                    "icd": "icd10",
+                    "score": "charlson",
+                    "variant": "shmi",
+                    "weighting": "shmi",
+                },
+                "max_ccs": 110,
+            },
+            {
+                "args": {
+                    "icd": "icd10",
+                    "score": "charlson",
+                    "variant": "quan",
+                    "weighting": "charlson",
+                },
+                "max_ccs": 29,
+            },
+            {
+                "args": {
+                    "icd": "icd10",
+                    "score": "charlson",
+                    "variant": "quan",
+                    "weighting": "quan",cd str
+                },
+                "max_ccs": 22,
+            },
+        ]
+        self.number_of_runs = 20
+
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        pass
 
-    def test_000_something(self):
-        """Test something."""
 
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'comorbidipy.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    def test_comorbidity_calculator(self):
+
+        for x in self.test_args:
+
+            kwargs = x["args"]
+            max_ccs = x["max_ccs"]
+
+            for i in range(self.number_of_runs):
+
+                df = generate_synthetic_icd10_data(**kwargs)
+                ccs = comorbidity(df, **kwargs)
+
+                self.assertTrue(all(ccs["comorbidity_score"] == max_ccs))
+
+
+if __name__ == "__main__":
+    unittest.main()
