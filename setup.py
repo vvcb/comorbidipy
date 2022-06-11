431f76e77138d543dcf1c1fbc891dacb82d06ea4
vvcb <vvcb.n1@gmail.com>
2022-06-11 10:48:13 +0100

    Update bumpversion config again
    Trying to get this working with GitHub Actions

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vvcb/comorbidipy",
-    version="version=0.4.3",
+    version="0.4.3",
     zip_safe=False,
 )
